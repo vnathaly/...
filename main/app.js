35431da74326d5eba638613f5d3ee01303ee8f99
Nathaly Victoriano <nathalyvictorianodiaz2020fb@gmail.com>
2024-07-31 20:08:23 -0700

    Added a some changes

--- main/app.js
+++ main/app.js
@@ -2,19 +2,19 @@ const socket = io();
 
 //Elements That I need to the DOM
 const message = document.getElementById('message');
-const username = document.getElementById('username');
-const btn = document.getElementById('send');
+const username = document.getElementById('usernamedata');
+const btn = document.getElementById('button');
 const viewMessage = document.getElementById('view');
 const actions = document.getElementById('actions');
 
 btn.addEventListener('click', () => {
   socket.emit('ElSeñorMensaje',{
-    username: username.value,
+    username: usernamedata.value,
     message: message.value
   })
 });
 
 socket.on('mensajeServidor', (data)=> {
-  viewMessage.innerHTML += `<p><strong> ${data.username} </strong></p>: ${data.message}`
+  viewMessage.innerHTML += `<p><strong> ${data.usernamedata} </strong></p>: ${data.message}`
 })
 
